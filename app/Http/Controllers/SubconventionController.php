265afca43976210eae4f2f4d9d515c953268e6fb
MarcusAfolabi <biormarcus@gmail.com>
2023-11-29 11:46:09 +0100

    updated

--- app/Http/Controllers/SubconventionController.php
+++ app/Http/Controllers/SubconventionController.php
@@ -16,7 +16,7 @@ class SubconventionController extends Controller
 { 
     public function __construct()
     {
-        $this->middleware(['auth', 'verified', 'admin'])->except(['index', 'store', 'subpayment']);
+        // $this->middleware(['auth', 'verified'])->except(['index', 'store', 'subpayment']);
     }
     public function index(Request $request)
     {
