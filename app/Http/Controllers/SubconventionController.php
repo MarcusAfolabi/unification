c0d825bc410138e8c2d0f1ba388b56fa5e34cb94
MarcusAfolabi <biormarcus@gmail.com>
2024-10-21 17:57:09 +0300

    updated

--- app/Http/Controllers/SubconventionController.php
+++ app/Http/Controllers/SubconventionController.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Notification;
 use App\Notifications\ConventionNotification;
 
 class SubconventionController extends Controller
-{ 
+{
     public function __construct()
     {
         $this->middleware(['auth', 'verified'])->except(['index', 'store', 'subpayment']);
@@ -21,51 +21,49 @@ class SubconventionController extends Controller
     public function index(Request $request)
     {
         return view('subconvention.index');
-       
-    } 
+    }
     public function list(Request $request)
     {
-            if ($request->subconvention) {
-                $subconventions = Subconvention::where('firstname', 'like', '%' . $request->subconventions . '%')
-                    ->orWhere('email', 'like', '%' . $request->subconventions . '%')->latest()->paginate(30);
-            } else {
-                $subconventions = Subconvention::latest()->paginate(30);
-            }
-            $role = Auth::user()->role;
-    
-            if ($role == 'admin') {
+        if ($request->subconvention) {
+            $subconventions = Subconvention::where('firstname', 'like', '%' . $request->subconventions . '%')
+                ->orWhere('email', 'like', '%' . $request->subconventions . '%')->latest()->paginate(30);
+        } else {
+            $subconventions = Subconvention::latest()->paginate(30);
+        }
+        $role = Auth::user()->role;
+
+        if ($role == 'admin') {
             $subconventions = Subconvention::latest()->paginate(30);
             return view('subconvention.list', compact('subconventions'));
-            }else{
-                abort(419);
-            }
-
-    }
-
-    public function subcard(){
-        if($myidcards = Convention::where('email', auth()->user()->email)->latest()->take(1)->get()){
-            dd($myidcards);
-        return view('subconvention.idcard', compact('myidcards'));
-        }else{
-        // return redirect('subconvention.index');
-        return redirect(route('subconvention.index'))->with('status', 'Register to get your ID CARD');
-
+        } else {
+            abort(419);
         }
     }
- 
+
+    public function subcard()
+    {
+        if ($myidcards = Subconvention::where('email', auth()->user()->email)->latest()->take(1)->get()) {
+            info($myidcards);
+            return view('subconvention.idcard', compact('myidcards'));
+        } else {
+            // return redirect('subconvention.index');
+            return redirect(route('subconvention.index'))->with('status', 'Register to get your ID CARD');
+        }
+    }
+
     public function store(Request $request)
     {
         $request->validate([
             "email" => 'required|unique:subconventions|allowed_email_domain|string',
             "lastname" => 'required|max:255|string',
-            "firstname" => 'required|max:255|string', 
+            "firstname" => 'required|max:255|string',
             "phone" => 'required|unique:subconventions|string',
             "gender" => 'required|string',
             "academic_status" => 'required|string',
             "fellowship_status" => 'required|string',
             "unit_id" => 'required|string',
             "fellowship_id" => 'required|string',
-            
+
         ]);
         $firstname = $request->input('firstname');
         $lastname = $request->input('lastname');
@@ -75,8 +73,8 @@ class SubconventionController extends Controller
         $academic_status = $request->input('academic_status');
         $fellowship_status = $request->input('fellowship_status');
         $unit_id = $request->input('unit_id');
-        $fellowship_id = $request->input('fellowship_id'); 
-        
+        $fellowship_id = $request->input('fellowship_id');
+
 
         $subconvention = new Subconvention();
         $subconvention->email = $email;
@@ -87,7 +85,7 @@ class SubconventionController extends Controller
         $subconvention->academic_status = $academic_status;
         $subconvention->fellowship_status = $fellowship_status;
         $subconvention->unit_id = $unit_id;
-        $subconvention->fellowship_id = $fellowship_id; 
+        $subconvention->fellowship_id = $fellowship_id;
         $subconvention->save();
 
         // Notification::route('mail', [
@@ -98,20 +96,19 @@ class SubconventionController extends Controller
 
         return redirect(route('convention.payment'))->with('status', 'Your Sub Convention Registration is Received Successfully. Proceed to make payment if you have not');
     }
- 
+
     public function show($id)
     {
         //
     }
- 
-    
+
+
     public function subpayment()
     {
         // return view('convention.payment');
         return redirect(route('convention.payment'));
+    }
 
-    } 
-    
     public function destroy(Subconvention $subconvention)
     {
         $subconvention->delete();
--- resources/views/subconvention/idcard.blade.php
+++ resources/views/subconvention/idcard.blade.php
@@ -1,22 +1,23 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sub Convention ID CARD</title>
-    <link rel="stylesheet" href="{{ asset('assets/card.css') }}" />
-    <script src="https://kit.fontawesome.com/66aa7c98b3.js" crossorigin="anonymous"></script>    
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Sub Convention ID CARD</title>
+	<link rel="stylesheet" href="{{ asset('assets/card.css') }}" />
+	<script src="https://kit.fontawesome.com/66aa7c98b3.js" crossorigin="anonymous"></script>
 </head>
 
-<body> 
-<div class="id-card-tag"></div>
+<body>
+	<div class="id-card-tag"></div>
 	<div class="id-card-tag-strip"></div>
 	<div class="id-card-hook"></div>
-    @foreach ($myidcards as $myidcard)
+	@foreach ($myidcards as $myidcard)
 	<div class="id-card-holder">
 		<div class="id-card">
-        <p>isokan-2022-00{{ $myidcard->id }}</p> 
+			<p>
+			<p>isokan-{{ date('Y') }}-00--{{ $myidcard->id }}</p>
 			<div class="header">
 				<img src="{{ asset('assets/images/favicon/mstile-150x150.png') }}">
 			</div>
@@ -24,23 +25,24 @@
 				<img src="{{ asset($myidcard->profile_image) }}">
 			</div>
 			<h2>{{ $myidcard->firstname }} {{ $myidcard->lastname }}
-                <br>{{ $myidcard->unificationStatus }}</h2>
-                <br>{{ $myidcard->unificationCurrentPost}}</h2>
+				<br>{{ $myidcard->unificationStatus }}
+			</h2>
+			<br>{{ $myidcard->unificationCurrentPost}}</h2>
 			<hr>
 			<h3>{{ $myidcard->yourFellowship }} | {{ $myidcard->levelInSchool }}<br />
-                <strong>
-                Coker House </strong></h3>
+				<strong>
+					Coker House </strong>
+			</h3>
 			<hr>
-			<h6> <small> Central Office:  www.cnsunification.org  
-			<br>No 13, Iyaniwura Street, Everbest Busstop, Meran, Lagos.  
-			<br>helpdesk@cnsunification.org <br> 08028554541, 08036506376
-			<br>
-            <br> If lost, pls call 
-			<br>Phone: {{ $myidcard->phoneNumber }} <br> Email: {{ $myidcard->email }}</small></h6>
-
+			<h6> <small> Central Office: www.cnsunification.org
+					<br>No 13, Iyaniwura Street, Everbest Busstop, Meran, Lagos.
+					<br>helpdesk@cnsunification.org <br> 08028554541, 08036506376
+					<br>
+					<br> If lost, pls call
+					<br>Phone: {{ $myidcard->phoneNumber }} <br> Email: {{ $myidcard->email }}</small></h6>
 		</div>
 	</div>
-    @endforeach
+	@endforeach
 </body>
 
-</html>  
+</html>
