6237eca779fabe66fa0344ee9082020f8c2faf40
MarcusAfolabi <biormarcus@gmail.com>
2024-11-05 20:09:09 +0300

    updated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -76,13 +76,17 @@
                                         </g>
                                     </g>
                                 </svg>Logout </a>
-                                <a wire:navigate href="{{ route('logout') }}" id="night-mode"
+                            @persist('mode')
+                            <a wire:navigate href="#" id="night-mode"
                                 class="btn-night-mode"><svg xmlns="http://www.w3.org/2000/svg"
                                     viewBox="0 0 20 20" fill="currentColor">
                                     <path
                                         d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                                </svg>Night mode <span class="btn-night-mode-switch"><span
-                                        class="uk-switch-button"></span></span></a>
+                                </svg>
+                                Night mode <span class="btn-night-mode-switch"><span
+                                        class="uk-switch-button"></span></span>
+                            </a>
+                            @endpersist
                             @else
                             <a wire:navigate href="{{ route('login') }}"><svg xmlns="http://www.w3.org/2000/svg"
                                     fill="none" viewBox="0 0 24 24" stroke="currentColor">
