48ede0a136b799c113ac09da8de1fc2500fabf1b
MarcusAfolabi <biormarcus@gmail.com>
2024-11-05 20:06:19 +0300

    updated

--- resources/views/subconvention/list.blade.php
+++ resources/views/subconvention/list.blade.php
@@ -65,7 +65,7 @@
                                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{{
                                             ++$key}}</td>
                                         <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                            <a href="#">Download ID Card</a>
+                                            <a class="btn btn-primary" href="#">Download ID Card</a>
                                         </td>
                                         <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             {{ $convention->lastname }} {{ $convention->firstname }}
