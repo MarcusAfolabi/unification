e3c8b010c4503bc99cdc77f46a89293f0a4f847c
MarcusAfolabi <biormarcus@gmail.com>
2024-11-05 20:16:34 +0300

    updated

--- app/Http/Controllers/SubconventionController.php
+++ app/Http/Controllers/SubconventionController.php
@@ -47,6 +47,7 @@ class SubconventionController extends Controller
     public function subcard(Request $request)
     {
         $email = Session::get('email') ?? $request->email;
+        dd($email);
         if ($myidcard = FourthSubConvention::where('email', $email)->first()) {
             // dd($myidcard);
             return view('subconvention.idcard', compact('myidcard'));
