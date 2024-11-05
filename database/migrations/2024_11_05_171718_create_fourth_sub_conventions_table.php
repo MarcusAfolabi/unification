a0d38393226c8214c8fdd11bf6a9f77386d63fcf
MarcusAfolabi <biormarcus@gmail.com>
2024-11-05 19:23:07 +0300

    updated

--- database/migrations/2024_11_05_171718_create_fourth_sub_conventions_table.php
+++ database/migrations/2024_11_05_171718_create_fourth_sub_conventions_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('fourth_sub_conventions', function (Blueprint $table) {
             $table->id();
+            $table->string('payment_proof');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('gender');
+            $table->string('phone', 11);
+            $table->string('fellowship_id');
+            $table->string('academic_status');
+            $table->string('fellowship_status');
+            $table->string('unit_id');
+            $table->string('profile_image');
             $table->timestamps();
         });
     }
