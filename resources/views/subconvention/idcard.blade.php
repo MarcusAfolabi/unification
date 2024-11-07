a143dbae81803226ea7fac2aa1ba60de26b90d40
MarcusAfolabi <biormarcus@gmail.com>
2024-11-07 08:58:32 +0300

    updated

--- resources/views/subconvention/idcard.blade.php
+++ resources/views/subconvention/idcard.blade.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -7,42 +8,109 @@
 	<title>Sub Convention ID CARD</title>
 	<link rel="stylesheet" href="{{ asset('assets/card.css') }}" />
 	<script src="https://kit.fontawesome.com/66aa7c98b3.js" crossorigin="anonymous"></script>
+	<style>
+		.container {
+			display: flex;
+			justify-content: space-around;
+			flex-wrap: wrap;
+		}
+
+		.meal-table {
+			width: 50%;
+			border-collapse: collapse;
+			margin: 20px;
+		}
+
+		.meal-table th,
+		.meal-table td {
+			border: 1px solid #ddd;
+			padding: 10px;
+			text-align: center;
+		}
+
+		.meal-table th {
+			background-color: #f2f2f2;
+			font-weight: bold;
+		}
+
+		.id-card-section {
+			width: 40%;
+			margin: 20px;
+		}
+	</style>
 </head>
 
 <body>
-	
-	<div class="id-card-tag"></div>
-	<div class="id-card-tag-strip"></div>
-	<div class="id-card-hook"></div>
-	@if ($myidcard)
-	<div class="id-card-holder">
-		<div class="id-card">
-			<p>
-			<p>isokan-{{ date('Y') }}-00--{{ $myidcard->id }}</p>
-			<div class="header">
-				<img src="{{ asset('assets/images/favicon/mstile-150x150.png') }}">
+
+	<div class="container">
+		<!-- Meal Schedule Table -->
+		<table class="meal-table">
+			<tr>
+				<th>Day</th>
+				<th>Breakfast</th>
+				<th>Lunch</th>
+				<th>Dinner</th>
+			</tr>
+			<tr>
+				<td>Thursday</td>
+				<td><input type="checkbox"></td>
+				<td><input type="checkbox"></td>
+				<td><input type="checkbox"></td>
+			</tr>
+			<tr>
+				<td>Friday</td>
+				<td><input type="checkbox"></td>
+				<td><input type="checkbox"></td>
+				<td><input type="checkbox"></td>
+			</tr>
+			<tr>
+				<td>Saturday</td>
+				<td><input type="checkbox"></td>
+				<td><input type="checkbox"></td>
+				<td><input type="checkbox"></td>
+			</tr>
+			<tr>
+				<td>Sunday</td>
+				<td><input type="checkbox"></td>
+				<td><input type="checkbox"></td>
+				<td><input type="checkbox"></td>
+			</tr>
+		</table>
+
+		<!-- ID Card Content -->
+		<div class="id-card-section">
+			<div class="id-card-tag"></div>
+			<div class="id-card-tag-strip"></div>
+			<div class="id-card-hook"></div>
+			@if ($myidcard)
+			<div class="id-card-holder">
+				<div class="id-card">
+					<p>isokan-{{ date('Y') }}-00--{{ $myidcard->id }}</p>
+					<div class="header">
+						<img src="{{ asset('assets/images/favicon/mstile-150x150.png') }}">
+					</div>
+					<div class="photo">
+						<img src="{{ asset($myidcard->profile_image) }}">
+					</div>
+					<h2>{{ $myidcard->firstname }} {{ $myidcard->lastname }}
+						<br>{{ $myidcard->fellowship_status }}
+					</h2>
+					<h2>{{ $myidcard->unit_id }}</h2>
+					<hr>
+					<h3>{{ $myidcard->fellowship_id }} <br> {{ $myidcard->academic_status }}<br />
+						<strong>{{ $houses->name }}</strong>
+					</h3>
+					<hr>
+					<h6><small>Central Office: www.cnsunification.org
+							<br>No 13, Iyaniwura Street, Everbest Busstop, Meran, Lagos.
+							<br>helpdesk@cnsunification.org <br> 08028554541, 08036506376
+							<br>If lost, pls call
+							<br>Phone: {{ $myidcard->phone }} <br>Email: {{ $myidcard->email }}</small></h6>
+				</div>
 			</div>
-			<div class="photo">
-				<img src="{{ asset($myidcard->profile_image) }}">
-			</div>
-			<h2>{{ $myidcard->firstname }} {{ $myidcard->lastname }}
-				<br>{{ $myidcard->fellowship_status }}
-			</h2>
-			<br>{{ $myidcard->unit_id}}</h2>
-			<hr>
-			<h3>{{ $myidcard->fellowship_id }} <br> {{ $myidcard->academic_status }}<br />
-				<strong>{{ $houses->name }}  </strong>
-			</h3>
-			<hr>
-			<h6> <small> Central Office: www.cnsunification.org
-					<br>No 13, Iyaniwura Street, Everbest Busstop, Meran, Lagos.
-					<br>helpdesk@cnsunification.org <br> 08028554541, 08036506376
-					<br>
-					<br> If lost, pls call
-					<br>Phone: {{ $myidcard->phone }} <br> Email: {{ $myidcard->email }}</small></h6>
+			@endif
 		</div>
 	</div>
-	@endif
 
 </body>
 
